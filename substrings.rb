cb6c928329b59ceabd7b3ae5214bdef8ee57a84d
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-03-05 14:08:45 -0600

    Finished

--- substrings.rb
+++ substrings.rb
@@ -3,13 +3,16 @@
 def substrings(sentence, dictionary)
   dictionary.reduce(Hash.new(0)) do |result, dictionary_word|
     #binding.pry
-    #if dictionary_word.include?(word)
-    if sentence.upcase.include?(dictionary_word.upcase)
-      result[dictionary_word] += 1
+    sentence.split(' ').each do |sentence_word|
+      if sentence_word.upcase.include?(dictionary_word.upcase)
+        result[dictionary_word] += 1
+      end
     end
     result
   end
 end
+
+#Test
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
